6523a2803f6d7d1d434f9a04ea2bd678f77226dc
SanjeevSinjali <tdkzbiru@gmail.com>
2026-04-08 13:03:25 +0545

    correct code

--- lib/oops/encapsulation/person_final.dart
+++ lib/oops/encapsulation/person_final.dart
@@ -1,43 +1,40 @@
-// class Person {
-//   // properties
-//   final String name;
-//   final int age;
-//   final bool status;
+class Person {
+  // properties
+  final String name;
+  final int age;
+  final bool status;
 
-//   //const = compile time 
-//   //final = runtime
+  // constructor
+  Person({required this.name, required this.age, required this.status});
 
-//   // constructor: no return type
-//   Person({required this.name, required this.age, required this.status})
-   
-//   // Named constructor with only name(defaults: age =0, status = false)\
-//   // Person.withName({required this.name})
-//   //   : age = 0;
-//   //     status = false;
+  // Named constructor with only name(defaults: age =0, status = false)
+  Person.withName({required this.name})
+      : age = 0,
+        status = false;
 
-//   //Named constructor with name and age(defaults: status = false)
-//   //Person.withNameAndAge({required this.name, required this.age})
-//   //  : status = false;
+  // Named constructor with name and age(defaults: status = false)
+  Person.withNameAndAge({required this.name, required this.age})
+      : status = false;
 
-//   @override
-//   String toString() {
-//     return "$name is $age years old is ${status} ";
-//   }
-// }
+  @override
+  String toString() {
+    return "$name is $age years old is $status";
+  }
+}
 
-// void main() {
-//   Person p = Person(name: "Sanjeev", age: 23, status: true);
-//   print(p);
+void main() {
+  Person p = Person(name: "Sanjeev", age: 23, status: true);
+  print(p);
 
-//   // Person p2 = Person.withName(name: "Sanjeev");
-//   // print(p2);
+  Person p2 = Person.withName(name: "Sanjeev");
+  print(p2);
 
-//   // Person p3 = Person.withNameAndAge(name: "Sanjeev",age: 23);
-//   // print(p3); 
+  Person p3 = Person.withNameAndAge(name: "Sanjeev", age: 23);
+  print(p3);
 
-//   //Using Getter and Setter
-//   p.name = "Bob";
-//   p.age = 25;
-//   p.status = true;
-//   print(p);
-// }
+  // p.name = "Bob";
+  // p.age = 25;
+  // p.status = true;
+
+  print(p);
+}
