f2ec7c4023eabb08f47e78fa945cdb7c1018a32d
SanjeevSinjali <tdkzbiru@gmail.com>
2026-04-08 13:02:04 +0545

    comment

--- lib/array/secondhighest.dart
+++ lib/array/secondhighest.dart
@@ -1,22 +1,22 @@
-void main(){
-  List<int> arr =[1,2,3,4,5,6,7,8,9,10];
+// void main(){
+//   List<int> arr =[1,2,3,4,5,6,7,8,9,10];
 
-  int first = arr[0];
-  int second = arr[0];
+//   int first = arr[0];
+//   int second = arr[0];
 
   
-}
+// }
 
-  // for(int i in arr){
-  //   int odd = 0;
-  //   int even = 0;
-  //   for(int i in arr){
-  //     if(i % 2 == 0){
-  //       even++;
-  //     } else {
-  //       odd++;
-  //     }
-  //   }
-  // }
-  // print("Odd : $odd and even :$Even");
-  // }
+// for(int i in arr){
+//   int odd = 0;
+//   int even = 0;
+//   for(int i in arr){
+//     if(i % 2 == 0){
+//       even++;
+//     } else {
+//       odd++;
+//     }
+//   }
+// }
+// print("Odd : $odd and even :$Even");
+// }
